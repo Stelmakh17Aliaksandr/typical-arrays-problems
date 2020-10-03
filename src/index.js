b12a904ff33b0f8f9c460295c16164ad4185b00b
Stelmakh17Aliaksandr <70840846+Stelmakh17Aliaksandr@users.noreply.github.com>
2020-10-03 16:59:09 +0300

    feat: task solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
-
 exports.min = function min (array) {
-  return 0;
+    let result = 0;
+    if (typeof array === `undefined` || array.length < 1) {
+        return result;
+    } else {
+        result = array[0];
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] < result) {
+                result = array[i]
+            }
+        }
+        return result;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    let result = 0;
+    if (typeof array === `undefined` || array.length < 1) {
+        return result;
+    } else {
+        result = array[0];
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] > result) {
+                result = array[i]
+            }
+        }
+        return result;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
-}
+    let result = 0;
+    if (typeof array === `undefined` || array.length < 1) {
+        return result;
+    } else {
+        for (let i = 0; i < array.length; i++) {
+            result = result + array[i]
+            }
+            result = result / array.length
+        }
+        return result;
+    }
+
+
